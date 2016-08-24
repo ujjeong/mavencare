f7437a3a345156701c3482cd17cddd0d4c922dee
Eugene Jeong <eugene@knowroaming.com>
2016-08-24 11:24:10 -0400

    mavencare assignment

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -14,4 +14,88 @@ angular.module('medicationReminderApp').controller('MainCtrl', function ($scope,
         $scope.$apply();
     }, 1000);
 
+    $scope.checkTime = function(m){
+    	var t = new Date("Tue Aug 24 2016 12:00:46 GMT-0400 (EDT)");
+    	
+    	/*
+			Checks each medication time with current time
+
+			- If current time is 5 min prior to medication time, "Mark as complete" button is enabled
+				-> status = beforeFive
+			- If medication time is the same as current time or current time is passed, calm-beep sound will be palyed for 5 minutes.
+			  Friendly warning will be shown
+				-> status = onTime
+			- If current time is 5 min passed for medication time, sharp-beep sound will be played and 'URGENT' icon will be displayed
+				-> status = aboutToGetMissed
+			- If current time has passsed 6 mins of medication time, sharp-beep sound will be stopped and medication will be marked as 'missed'
+			  Missed medication will be separated
+			  	-> status = missed
+
+		*/
+    	if(!m.completed && ( Date.parse(moment()) - Date.parse(m.time) ) >= 360000){
+    		return 'missed';
+    	}
+    	else if(!m.completed && ( Date.parse(moment()) - Date.parse(m.time) ) >= 300000 && ( Date.parse(moment()) - Date.parse(m.time) ) <= 360000){
+    		return 'aboutToGetMissed';
+    	}
+    	else if(!m.completed && ( Date.parse(moment()) - Date.parse(m.time) ) >= 0 && ( Date.parse(moment()) - Date.parse(m.time) ) <= 300000){
+    		return 'onTime';
+    	}
+    	else if(!m.completed && (Date.parse(m.time) - Date.parse(moment())) <= 300000 && (Date.parse(m.time) - Date.parse(moment())) >= 0){
+    		return 'beforeFive';
+    	}
+    	else{
+    		return 'notTimeYet';
+    	}
+	};
+});
+
+
+/*
+	Ctrl for datepicker
+
+	- Receives date from html and points it to /api/medications
+	- Returns corresponding medications based on Date
+
+*/	
+angular.module('medicationReminderApp').controller('DatepickerCtrl', function ($scope, $http) {
+
+	$scope.today = function() {
+		$scope.dt = moment();
+	};
+	$scope.currentDate = moment($scope.dt).format('MMMM Do, YYYY');
+	$scope.today();
+	$scope.repeatArray = [1];
+	$scope.showWeeks = false;
+
+	$scope.getMeds = function(){
+		var start = moment($scope.dt).format('MM/DD/YYYY'),
+        	end = moment($scope.dt).add(1, 'day').format('MM/DD/YYYY');
+		
+		$http.get('/api/medications?start=' + start + '&end=' + end).then(function (meds) {
+			$scope.$parent.meds = meds.data;
+			$scope.currentDate = moment($scope.dt).format('MMMM Do, YYYY');
+	    });
+	};
+
+  $scope.dateOptions = {
+    'year-format': '"yyyy"',
+    'starting-day': 1
+  };
+});
+
+/*
+	Ctrl for updating medication
+
+	- PUT request to /api/medications/:id
+	- Updates corresponding medications based on medication ID
+	
+*/
+angular.module('medicationReminderApp').controller('UpdateMedCtrl', function ($scope, $http){
+	$scope.updateMeds = function(m){
+		m.completed = !m.completed;
+		$http.put('/api/medications/' + m._id, m).then(function (meds) {
+			$scope.$parent.meds = meds.data;
+	    });
+	};
 });
--- client/app/main/main.controller.spec.js
+++ client/app/main/main.controller.spec.js
@@ -12,8 +12,12 @@ describe('Controller: MainCtrl', function () {
   // Initialize the controller and a mock scope
   beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
     $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('/api/things')
-      .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
+
+    var start = moment().format('MM/DD/YYYY'),
+        end = moment().add(1, 'day').format('MM/DD/YYYY');
+
+    $httpBackend.expectGET('/api/medications?start=' + start + '&end=' + end)
+      .respond(['completed', 'time', 'dosage', 'name']);
 
     scope = $rootScope.$new();
     MainCtrl = $controller('MainCtrl', {
@@ -23,6 +27,48 @@ describe('Controller: MainCtrl', function () {
 
   it('should attach a list of things to the scope', function () {
     $httpBackend.flush();
-    expect(scope.awesomeThings.length).toBe(4);
+    expect(scope.meds.length).toBe(4);
+  });
+});
+
+describe('Testing PUT method', function () {
+
+  // load the controller's module
+  beforeEach(module('medicationReminderApp'));
+
+  var UpdateMedCtrl,
+      scope,
+      $httpBackend;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
+    $httpBackend = _$httpBackend_;
+
+    $httpBackend.when('PUT', '/api/medications/57bb2797d9a7c24a74b6fd5c')
+      .respond(['completed', 'time', 'dosage', 'name']);
+
+    scope = $rootScope.$new();
+
+    UpdateMedCtrl = $controller('UpdateMedCtrl', {
+      '$scope': scope
+    });
+  }));
+
+  it('should be able to request PUT and get response back', function () {
+
+    var m = {
+      object: '106',
+      _id: '57bb2797d9a7c24a74b6fd5c',
+      dosage: '200 mL',
+      name: 'Medication 20',
+      time: '2016-08-25T01:49:05.454Z'
+    }
+
+    // updateMeds function performs PUT request
+    scope.updateMeds(m);
+
+    // Execute PUT request
+    $httpBackend.flush();
+    expect(scope.meds.length).toBe(4);
   });
 });
